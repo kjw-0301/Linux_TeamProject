f13a37a8a3a62fb18e1df435ae6d74e32f077c64
Kim Jong Won <147674469+kjw-0301@users.noreply.github.com>
2024-09-06 11:18:38 +0900

    Update Ultrasonic_led.c

--- Ultrasonic_led.c
+++ Ultrasonic_led.c
@@ -10,10 +10,19 @@
 #define ECHO2 27
 #define LED2  22
 
+#define SEG_A 0
+#define SEG_B 1
+#define SEG_C 19
+#define SEG_D 16
+#define SEG_E 26
+#define SEG_F 20
+#define SEG_G 21
+
 double distance1 = 0.0;
 double distance2 = 0.0;
 int led1_vacancy = 0;	// 1 == vacant, 0 == occupied
 int led2_vacancy = 0;
+int vacancy_count = 0;
 
 // Initializes the ultrasonic sensor by setting up the TRIG, ECHO, and LED pins.
 void initSensor(int trig, int echo, int led)
@@ -25,6 +34,17 @@ void initSensor(int trig, int echo, int led)
     digitalWrite(trig, LOW);
 }
 
+void init7Segment()
+{
+    pinMode(SEG_A, OUTPUT);
+    pinMode(SEG_B, OUTPUT);
+    pinMode(SEG_C, OUTPUT);
+    pinMode(SEG_D, OUTPUT);
+    pinMode(SEG_E, OUTPUT);
+    pinMode(SEG_F, OUTPUT);
+    pinMode(SEG_G, OUTPUT);
+}
+
 // Initializes the wiringPi library and sets up the ultrasonic sensors, and LED
 void setup()
 {
@@ -34,6 +54,107 @@ void setup()
     initSensor(TRIG2, ECHO2, LED2);
 
     delay(30);
+}
+
+void displayNumber(int number)
+{
+    delay(500);
+    switch (number)
+    {
+        case 0:
+            digitalWrite(SEG_A, HIGH);
+            digitalWrite(SEG_B, HIGH);
+            digitalWrite(SEG_C, HIGH);
+            digitalWrite(SEG_D, HIGH);
+            digitalWrite(SEG_E, HIGH);
+            digitalWrite(SEG_F, HIGH);
+            digitalWrite(SEG_G, LOW);
+            break;
+        case 1:
+            digitalWrite(SEG_A, LOW);
+            digitalWrite(SEG_B, HIGH);
+            digitalWrite(SEG_C, HIGH);
+            digitalWrite(SEG_D, LOW);
+            digitalWrite(SEG_E, LOW);
+            digitalWrite(SEG_F, LOW);
+            digitalWrite(SEG_G, LOW);
+            break;
+        case 2:
+            digitalWrite(SEG_A, HIGH);
+            digitalWrite(SEG_B, HIGH);
+            digitalWrite(SEG_C, LOW);
+            digitalWrite(SEG_D, HIGH);
+            digitalWrite(SEG_E, HIGH);
+            digitalWrite(SEG_F, LOW);
+            digitalWrite(SEG_G, HIGH);
+            break;
+        case 3:
+            digitalWrite(SEG_A, HIGH);
+            digitalWrite(SEG_B, HIGH);
+            digitalWrite(SEG_C, HIGH);
+            digitalWrite(SEG_D, HIGH);
+            digitalWrite(SEG_E, LOW);
+            digitalWrite(SEG_F, LOW);
+            digitalWrite(SEG_G, HIGH);
+            break;
+        case 4:
+            digitalWrite(SEG_A, LOW);
+            digitalWrite(SEG_B, HIGH);
+            digitalWrite(SEG_C, HIGH);
+            digitalWrite(SEG_D, LOW);
+            digitalWrite(SEG_E, LOW);
+            digitalWrite(SEG_F, HIGH);
+            digitalWrite(SEG_G, HIGH);
+            break;
+        case 5:
+            digitalWrite(SEG_A, HIGH);
+            digitalWrite(SEG_B, LOW);
+            digitalWrite(SEG_C, HIGH);
+            digitalWrite(SEG_D, HIGH);
+            digitalWrite(SEG_E, LOW);
+            digitalWrite(SEG_F, HIGH);
+            digitalWrite(SEG_G, HIGH);
+            break;
+        case 6:
+            digitalWrite(SEG_A, HIGH);
+            digitalWrite(SEG_B, LOW);
+            digitalWrite(SEG_C, HIGH);
+            digitalWrite(SEG_D, HIGH);
+            digitalWrite(SEG_E, HIGH);
+            digitalWrite(SEG_F, HIGH);
+            digitalWrite(SEG_G, HIGH);
+            break;
+        case 7:
+            digitalWrite(SEG_A, HIGH);
+            digitalWrite(SEG_B, HIGH);
+            digitalWrite(SEG_C, HIGH);
+            digitalWrite(SEG_D, LOW);
+            digitalWrite(SEG_E, LOW);
+            digitalWrite(SEG_F, LOW);
+            digitalWrite(SEG_G, LOW);
+            break;
+        case 8:
+            digitalWrite(SEG_A, HIGH);
+            digitalWrite(SEG_B, HIGH);
+            digitalWrite(SEG_C, HIGH);
+            digitalWrite(SEG_D, HIGH);
+            digitalWrite(SEG_E, HIGH);
+            digitalWrite(SEG_F, HIGH);
+            digitalWrite(SEG_G, HIGH);
+            break;
+        case 9:
+            digitalWrite(SEG_A, HIGH);
+            digitalWrite(SEG_B, HIGH);
+            digitalWrite(SEG_C, HIGH);
+            digitalWrite(SEG_D, HIGH);
+            digitalWrite(SEG_E, LOW);
+            digitalWrite(SEG_F, HIGH);
+            digitalWrite(SEG_G, HIGH);
+            break;
+        default:
+           
+            break;
+    }
 }
 
 double getDistance(int trig, int echo)
@@ -83,4 +204,5 @@ void controlLED(int led, double distance, double threshold)
         if (led == LED1) led1_vacancy = 1;
         if (led == LED2) led2_vacancy = 1;
     }
+    printf("LED1 vacancy: %d, LED2 vacancy: %d\n", led1_vacancy, led2_vacancy);
 }
